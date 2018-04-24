ed97a6bb5b32d539316523576ea4ccdd32d6fdab
Ltrmex <maciekmajchrzak23@wp.pl>
2018-04-24 03:30:27 +0100

    camera follows the player

---
+++ Roll Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    //  Variables
+    public GameObject player;   //  reference to the player
+    private Vector3 offset; //  offset value of the camera
+
+    //  Initialization
+    void Start() {
+        //  Set offset
+        offset = transform.position - player.transform.position;
+    }   //  Start()
+
+    //  Guaranteed to run after all items were processed in update
+    void LateUpdate() {
+        //  Transform position of the camera to the position of the player + offset
+        transform.position = player.transform.position + offset;
+    }   //  LateUpdate()
+}   //  CameraController
