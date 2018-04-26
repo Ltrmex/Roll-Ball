a2dc7694635e6201174832014bc72bdd78f4ac4f
Ltrmex <maciekmajchrzak23@wp.pl>
2018-04-26 20:41:57 +0100

    placed pick up objects

---
+++ Roll Ball/Assets/Scripts/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+//  Class responsible for rotation of a gameobject
+public class Rotate : MonoBehaviour {
+
+    //  Update gets called each frame
+    void Update() {
+        //  Use transform to rotate object this script is attached to
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }   //  Update()
+}   //  Rotate
